b006398f9dd46e700793fcc8296a61091e143d8a
Zubair Khan <zubair.khan@gaditek.com>
2021-10-03 00:56:09 +0500

    deploye wordpress salts generator

---
+++ src/Random/Generator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WPSalts\Random;
+
+use RandomLib\Factory;
+
+/**
+ * A simple class that will generate a random 64 character string. This class
+ * is based on the paragonie/random-lib which is a fork of the PHP
+ * ircmaxell/random-lib which no longer seems to be in support.
+ *
+ * @author Zubidev <zubidev@gmail.com>
+ */
+class Generator
+{
+    /**
+     * @var Factory $factory
+     */
+    private $factory;
+
+    /**
+     * List of characters to be used in the salt generation
+     *
+     * @var string $chars
+     */
+    private $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&()*+,-./:;<=>?@[]^_`{|}~';
+
+    /**
+     * @param Factory $factory
+     */
+    public function __construct(Factory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * Generates 64 character salt string
+     *
+     * @return string
+     */
+    public function salt(): string
+    {
+        return $this->factory->getMediumStrengthGenerator()
+            ->generateString(64, $this->chars);
+    }
+}
---
+++ src/Salts.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace WPSalts;
+
+use WPSalts\Random\Generator;
+use RandomLib\Factory;
+
+/**
+ * Class that generates WordPress salts and keys used in authentication and
+ * security. Generates the salts and keys in the traditional 'define'
+ * WordPress manner and the new Composer driven DotEnv format.
+ *
+ * @author Zubidev <zubidev@gmail.com>
+ */
+class Salts
+{
+    /**
+     * Generate and return all the WordPress salts as an array.
+     *
+     * @return array
+     */
+    public function wordPressSalts(): array
+    {
+        $generator = new Generator(new Factory);
+
+        $salts['AUTH_KEY'] = $generator->salt();
+        $salts['SECURE_AUTH_KEY'] = $generator->salt();
+        $salts['LOGGED_IN_KEY'] = $generator->salt();
+        $salts['NONCE_KEY'] = $generator->salt();
+        $salts['AUTH_SALT'] = $generator->salt();
+        $salts['SECURE_AUTH_SALT'] = $generator->salt();
+        $salts['LOGGED_IN_SALT'] = $generator->salt();
+        $salts['NONCE_SALT'] = $generator->salt();
+
+        return $salts;
+    }
+
+    /**
+     * Gets an array of WordPress salts and then reduces them to a string for
+     * output to the CLI. Returns them in the traditional WordPress define
+     * format used in wp-config.php files.
+     *
+     * @return string
+     */
+    public function traditional(): string
+    {
+        $salts = $this->wordPressSalts();
+
+        return array_reduce(array_keys($salts), function ($saltsString, $key) use ($salts) {
+            $saltsString .= "define('$key', '$salts[$key]');" . PHP_EOL;
+
+            return $saltsString;
+        }, '');
+    }
+
+    /**
+     * Gets an array of WordPress salts and then reduces them to a string for
+     * output to the CLI. Returns them in the DotEnv format used in .env files.
+     *
+     * @return string
+     */
+    public function dotEnv(): string
+    {
+        $salts = $this->wordPressSalts();
+
+        return array_reduce(array_keys($salts), function ($saltsString, $key) use ($salts) {
+            $saltsString .= "$key = '$salts[$key]'" . PHP_EOL;
+
+            return $saltsString;
+        }, '');
+    }
+}
